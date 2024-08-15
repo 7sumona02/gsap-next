b80b0e5bac0242682b419f0d879d0f9a3d031928
7sumona02 <hawkingsumona8@gmail.com>
2024-08-15 15:04:07 +0530

    services

---
+++ components/Card.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { BsArrowRight } from 'react-icons/bs'
+
+interface CardProps {
+    title: string,
+    bgTitle: string,
+    cardImg: string,
+    cardURL: string,
+    colorURL: string,
+    cardBg: string,
+    iconColor: string,
+    iconBg: string,
+  }
+
+const Card: React.FC<CardProps> = ({
+    title, bgTitle, cardImg, cardURL, colorURL, cardBg, iconColor, iconBg
+}) => {
+  return (
+    <div className={`p-10 rounded-3xl border-2 border-b-8 border-black max-w-md flex justify-between ${cardBg}`}>
+      <div className='flex flex-col gap-6'>
+        <div className='text-black max-w-60'>
+            <h1 className={`${bgTitle} py-2 inline text-black text-xl font-bold leading-8`}>{title}</h1>
+        </div>
+        <div className='flex gap-2 items-center mt-6'>
+            <div className={`${iconBg} rounded-full p-4 size-4 flex items-center justify-center`}>
+                <div><BsArrowRight className={`text-xl -rotate-45 ${iconColor}`} /></div>
+            </div>
+            <Link href={cardURL} className={`font-medium ${colorURL} text-sm`}>Learn more</Link>
+        </div>
+      </div>
+      <div>
+        <Image src={cardImg} alt='Hero' width={400} height={400} className='w-[10vw]' />
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- components/Services.tsx
+++ components/Services.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from './Card'
 
 const Services = () => {
   return (
@@ -7,8 +8,50 @@ const Services = () => {
         <h2 className='text-2xl font-bold bg-green-300 py-2 px-2 inline'>Services</h2>
         <p className='font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sequi dolorem voluptatibus eveniet quos, ab eum iste saepe doloribus hic.</p>
       </div>
-      <div>
 
+      <div className='flex justify-start mt-20'>
+        <div className='grid grid-cols-2 gap-10'>
+            <Card
+                title='Search engine optimization'
+                cardBg='bg-white'
+                bgTitle='bg-green-300'
+                cardImg='/Hero.svg'
+                cardURL='/'
+                colorURL='text-black'
+                iconBg='bg-black'
+                iconColor='text-white'
+                />
+            <Card
+                title='Pay per click advertising'
+                cardBg='bg-black'
+                bgTitle='bg-white'
+                cardImg='/Hero2.svg'
+                cardURL='/'
+                colorURL='text-white'
+                iconBg='bg-white'
+                iconColor='text-black'
+                />
+            <Card
+                title='Pay per click advertising'
+                cardBg='bg-black'
+                bgTitle='bg-white'
+                cardImg='/Hero2.svg'
+                cardURL='/'
+                colorURL='text-white'
+                iconBg='bg-white'
+                iconColor='text-black'
+                />
+            <Card
+                title='Search engine optimization'
+                cardBg='bg-white'
+                bgTitle='bg-green-300'
+                cardImg='/Hero.svg'
+                cardURL='/'
+                colorURL='text-black'
+                iconBg='bg-black'
+                iconColor='text-white'
+                />
+        </div>
       </div>
     </div>
   )
