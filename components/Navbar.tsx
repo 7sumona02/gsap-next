d7534188b7102323162d2adff5e5980e7414b892
7sumona02 <hawkingsumona8@gmail.com>
2024-08-15 15:11:28 +0530

    Navbar design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import { PiStarFourFill } from 'react-icons/pi'
 
 const Navbar = () => {
   return (
-    <div className='p-10'>
+    <div className='px-10 py-6 sticky inset-x-0 top-0 z-500 w-full bg-neutral-50/70 backdrop-blur-lg transitional-all'>
       <div className='text-xl text-black flex items-center justify-between'>
         <div className='flex items-center'>
             <div className='mr-2'><PiStarFourFill className='rotate-45 text-2xl' /></div>
@@ -12,7 +12,7 @@ const Navbar = () => {
         </div>
         <div className='flex gap-6 items-center text-lg font-medium'>
             <Link href='/'>About us</Link>
-            <Link href='/'>Services</Link>
+            <Link href='#services'>Services</Link>
             <Link href='/'>Pricing</Link>
             <Link href='/'>Blog</Link>
             <button className='py-2 px-4 text-sm border-2 border-black bg-neutral-200'>Request a Demo</button>
--- components/Services.tsx
+++ components/Services.tsx
@@ -3,7 +3,7 @@ import Card from './Card'
 
 const Services = () => {
   return (
-    <div className='p-20 bg-neutral-50'>
+    <div className='p-20 bg-neutral-50' id='services'>
       <div className='text-black flex gap-10 max-w-3xl'>
         <h2 className='text-2xl font-bold bg-green-300 py-2 px-2 inline'>Services</h2>
         <p className='font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sequi dolorem voluptatibus eveniet quos, ab eum iste saepe doloribus hic.</p>
